0ca45c6a75c46001c59cc4ad4b2d54a353daf6ac
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-11-18 23:31:06 +0100

    First commit

---
+++ src/main/java/svinstvo/b4b/B4bApplication.java
@@ -0,0 +1,13 @@
+package svinstvo.b4b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B4bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B4bApplication.class, args);
+	}
+
+}
